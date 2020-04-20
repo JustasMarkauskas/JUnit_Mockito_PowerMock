dc48f60c108c983496c85a4bee03b112051d860f
Justas M <justas.markauskas@gmail.com>
2020-04-20 20:27:47 +0300

    Junit

--- junit/src/test/java/com/in28minutes/junit/helper/AllTests.java
+++ junit/src/test/java/com/in28minutes/junit/helper/AllTests.java
@@ -5,7 +5,9 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ArraysCompareTest.class, QuickBeforeAfterTest.class, StringHelperTest.class })
+@SuiteClasses({ ArraysCompareTest.class, 
+				QuickBeforeAfterTest.class, 
+				StringHelperTest.class })
 public class AllTests {
 
 }
